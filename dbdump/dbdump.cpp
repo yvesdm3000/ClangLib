6f1a26990fc9f35757bf9cca9d391376931396e8
Yves De Muyter <yves@alfavisio.be>
2017-03-21 19:38:23 +0000

    Added tokencategory to output + fix build

--- dbdump/dbdump.cpp
+++ dbdump/dbdump.cpp
@@ -11,7 +11,7 @@
 #endif
 
 
-std::ostream& operator<<(std::ostream& o, ClTokenType val)
+std::ostream& operator<<(std::ostream& o, const ClTokenType val)
 {
     int valInt = val;
     switch(val)
@@ -59,6 +59,141 @@ std::ostream& operator<<(std::ostream& o, ClTokenType val)
     return o;
 };
 
+std::ostream& operator<<(std::ostream& o, const ClTokenCategory val)
+{
+    switch (val)
+    {
+        case tcClassFolder:
+            o<<"Class folder";
+            break;
+    case tcClass:
+        o<<"Class";
+        break;
+    case tcClassPrivate:
+        o<<"Private class";
+        break;
+    case tcClassProtected:
+        o<<"Protected class";
+        break;
+    case tcClassPublic:
+        o<<"Public class";
+        break;
+    case tcCtorPrivate:
+        o<<"Private constructor";
+        break;
+    case tcCtorProtected:
+        o<<"Protected constructor";
+        break;
+    case tcCtorPublic:
+        o<<"Public constructor";
+        break;
+    case tcDtorPrivate:
+        o<<"Private destructor";
+        break;
+    case tcDtorProtected:
+        o<<"Protected destructor";
+        break;
+    case tcDtorPublic:
+        o<<"Public destructor";
+        break;
+    case tcFuncPrivate:
+        o<<"Private function";
+        break;
+    case tcFuncProtected:
+        o<<"Protected function";
+        break;
+    case tcFuncPublic:
+        o<<"Public function";
+        break;
+    case tcVarPrivate:
+        o<<"Private variable";
+        break;
+    case tcVarProtected:
+        o<<"Protected variable";
+        break;
+    case tcVarPublic:
+        o<<"Public variable";
+        break;
+    case tcMacroDef:
+        o<<"Macro definition";
+        break;
+    case tcEnum:
+        o<<"Enum";
+        break;
+    case tcEnumPrivate:
+        o<<"Private enum";
+        break;
+    case tcEnumProtected:
+        o<<"Protected enum";
+        break;
+    case tcEnumPublic:
+        o<<"Public enum";
+        break;
+    case tcEnumerator:
+        o<<"Enumerator";
+        break;
+    case tcNamespace:
+        o<<"Namespace";
+        break;
+    case tcTypedef:
+        o<<"Typedef";
+        break;
+    case tcTypedefPrivate:
+        o<<"Private private";
+        break;
+    case tcTypedefProtected:
+        o<<"Protected typedef";
+        break;
+    case tcTypedefPublic:
+        o<<"Public typedef";
+        break;
+    case tcSymbolsFolder:
+        o<<"Symbols folder";
+        break;
+    case tcVarsFolder:
+        o<<"Vars folder";
+        break;
+    case tcFuncsFolder:
+        o<<"Funcs folder";
+        break;
+    case tcEnumsFolder:
+        o<<"Enums folder";
+        break;
+    case tcPreprocFolder:
+        o<<"Preproc folder";
+        break;
+    case tcOthersFolder:
+        o<<"Others folder";
+        break;
+    case tcTypedefFolder:
+        o<<"Typedef folder";
+        break;
+    case tcMacroUse:
+        o<<"Macro use";
+        break;
+    case tcMacroPrivate:
+        o<<"Private macro";
+        break;
+    case tcMacroProtected:
+        o<<"Protected macro";
+        break;
+    case tcMacroPublic:
+        o<<"Public macro";
+        break;
+    case tcMacroFolder:
+        o<<"Macro folder";
+        break;
+    case tcLangKeyword: // added
+        o<<"Language keyword";
+        break;
+    default:
+    case tcNone: ;
+        o<<"None";
+        break;
+    }
+    return o;
+}
+
 ClFilenameDatabase g_FilenameDatabase;
 
 class SymbolDB : public IPersistentTokenIndexDatabase
@@ -83,7 +218,7 @@ public:
         return ret;
     }
     std::vector<ClIndexToken> m_TokenList;
-    std::map<std::pair<wxString,wxString>, ClIndexToken*> m_TokenMap;
+    std::map<std::pair<ClIdentifierString,ClUSRString>, ClIndexToken*> m_TokenMap;
 };
 
 class ListSymbols : public IPersistentTokenIndexDatabase
@@ -106,7 +241,7 @@ public:
     {
         unsigned count = 0;
         ClFileId fileFilterId = wxNOT_FOUND;
-        if (!m_FilenameFilter.IsEmpty())
+        if (!m_FilenameFilter.empty())
         {
             fileFilterId = g_FilenameDatabase.GetFilenameId( m_FilenameFilter );
             if (fileFilterId == wxNOT_FOUND)
@@ -122,20 +257,22 @@ public:
             }
         }
         OutputIndent( indent );
-        std::cout<<"Token:                "<<token.identifier.utf8_str()<<std::endl;
+        std::cout<<"Token:                "<<token.identifier<<std::endl;
         OutputIndent( indent );
-        std::cout<<"  Display Name:       "<<token.displayName.utf8_str()<<std::endl;
+        std::cout<<"  Display Name:       "<<token.displayName.ToUTF8().data()<<std::endl;
         OutputIndent( indent );
-        std::cout<<"  USR:                "<<token.USR.utf8_str()<<std::endl;
-        if (!token.scope.first.IsEmpty())
+        std::cout<<"  USR:                "<<token.USR<<std::endl;
+        OutputIndent( indent );
+        std::cout<<"  Category:           "<<token.category<<std::endl;
+        if (!token.semanticScope.first.empty())
         {
             OutputIndent( indent );
-            std::cout<<"  Scope identifier:   "<<token.scope.first.utf8_str()<<std::endl;
+            std::cout<<"  Scope identifier:   "<<token.semanticScope.first<<std::endl;
             OutputIndent( indent );
-            std::cout<<"        USR:          "<<token.scope.second.utf8_str()<<std::endl;
+            std::cout<<"        USR:          "<<token.semanticScope.second<<std::endl;
             if (m_bResolveScope&&m_pSymbolDB)
             {
-                std::map<std::pair<wxString,wxString>, ClIndexToken*>::const_iterator scopeIt = m_pSymbolDB->m_TokenMap.find( std::make_pair( token.scope.first, token.scope.second ) );
+                std::map<std::pair<ClIdentifierString,ClUSRString>, ClIndexToken*>::const_iterator scopeIt = m_pSymbolDB->m_TokenMap.find( std::make_pair( token.semanticScope.first, token.semanticScope.second ) );
                 if (scopeIt != m_pSymbolDB->m_TokenMap.end())
                 {
                     OutputToken( indent+4, *scopeIt->second );
@@ -146,15 +283,15 @@ public:
         {
             OutputIndent( indent );
             std::cout<<"  Parents:"<<std::endl;
-            for (std::vector<std::pair<wxString,wxString> >::const_iterator it = token.parentTokenList.begin(); it != token.parentTokenList.end(); ++it)
+            for (std::vector<std::pair<ClIdentifierString,ClUSRString> >::const_iterator it = token.parentTokenList.begin(); it != token.parentTokenList.end(); ++it)
             {
                 OutputIndent( indent );
-                std::cout<<"    Identifier:       "<<it->first.utf8_str()<<std::endl;
+                std::cout<<"    Identifier:       "<<it->first<<std::endl;
                 OutputIndent( indent );
-                std::cout<<"    USR:              "<<it->second.utf8_str()<<std::endl;
+                std::cout<<"    USR:              "<<it->second<<std::endl;
                 if (m_bResolveParents&&m_pSymbolDB)
                 {
-                    std::map<std::pair<wxString,wxString>, ClIndexToken*>::const_iterator parentIt = m_pSymbolDB->m_TokenMap.find( std::make_pair( it->first, it->second ) );
+                    std::map<std::pair<ClIdentifierString,ClUSRString>, ClIndexToken*>::const_iterator parentIt = m_pSymbolDB->m_TokenMap.find( std::make_pair( it->first, it->second ) );
                     if (parentIt != m_pSymbolDB->m_TokenMap.end())
                     {
                         OutputToken(indent+4, *parentIt->second);
@@ -196,7 +333,7 @@ public:
                 OutputIndent( indent );
                 std::cout<<"    Range:            "<<std::dec<<it->range.beginLocation.line<<", "<<it->range.beginLocation.column<<" - "<<it->range.endLocation.line<<", "<<it->range.endLocation.column<<std::endl;
                 OutputIndent( indent );
-                std::cout<<"    File:             "<<g_FilenameDatabase.GetFilename( it->fileId ).utf8_str()<<std::endl;
+                std::cout<<"    File:             "<<g_FilenameDatabase.GetFilename( it->fileId )<<std::endl;
             }
         }
     }
@@ -222,7 +359,7 @@ public:
         }
     }
 
-    wxString m_FilenameFilter;
+    std::string m_FilenameFilter;
     const SymbolDB* m_pSymbolDB;
     bool m_bResolveParents;
     bool m_bResolveScope;
@@ -269,7 +406,7 @@ int main(int argc, char* argv[])
     {
         bool resolve = false;
         ListSymbols l;
-        l.m_FilenameFilter = fileFilter;
+        l.m_FilenameFilter = fileFilter.ToUTF8().data();
 
         if (parser.Found( wxT("resolveall") ))
         {
@@ -311,3 +448,11 @@ int main(int argc, char* argv[])
     }
     return 0;
 }
+
+
+
+
+
+
+
+
